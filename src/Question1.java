a5e2fffbd3d3ce5a888ba3eb9b9d46da5e7c3315
BT_1N3_12 <sbajnath6151@bths.edu>
2019-05-20 14:41:33 -0400

    things

--- src/Question1.java
+++ src/Question1.java
@@ -1,2 +1,62 @@
 public class Question1 {
+    /** Returns true if year is a leap year and false otherwise. */
+    private static boolean isLeapYear(int year)
+    {
+        return year%4==0;
+    }
+
+
+    /** Returns the number of leap years between year1 and year2, inclusive.
+     * Precondition: 0 <= year1 <= year2
+     */
+    public static int numberOfLeapYears(int year1, int year2)
+    {
+        int num = 0;
+        for(int i=year1; i<=year2; i++){
+           if(isLeapYear(i)){
+               num++;
+           }
+        }
+        return num;
+    }
+
+
+    /** Returns the value representing the day of the week for the first day of year,
+     * where 0 denotes Sunday, 1 denotes Monday, ..., and 6 denotes Saturday.
+     */
+    private static int firstDayOfYear(int year)
+    {
+        return year%7;
+    }
+
+
+    /** Returns n, where month, day, and year specify the nth day of the year.
+     * Returns 1 for January 1 (month = 1, day = 1) of any year.
+     * Precondition: The date represented by month, day, year is a valid date.
+     */
+    private static int dayOfYear(int month, int day, int year)
+    {
+        int num = day;
+        int[] days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if(isLeapYear(year)){
+            days[1]=29;
+        }
+        for(int i=0; i<month-1; i++){
+            num+=days[i];
+        }
+        return num;
+    }
+
+
+    /** Returns the value representing the day of the week for the given date
+     * (month, day, year), where 0 denotes Sunday, 1 denotes Monday, ...,
+     * and 6 denotes Saturday.
+     * Precondition: The date represented by month, day, year is a valid date.
+     */
+    public static int dayOfWeek(int month, int day, int year)
+    {
+        int num = firstDayOfYear(year);
+        num+= dayOfYear(month,day,year);
+        return num%7;
+    }
 }
